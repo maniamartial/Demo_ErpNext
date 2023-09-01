4d23485f544c39b244d4ce6c1805243353c5badb
maniamartial <martialmania19@gmail.com>
2023-09-01 18:17:17 +0300

    New doctype

---
+++ demo_app/teaching_module/doctype/doctype2/doctype2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Techmaniacc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Doctype2', {
+	// refresh: function(frm) {
+
+	// }
+});
